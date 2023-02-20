9eee52241aa504205cff9713e4088a7cb7f20526
sandeepkumarreddymaram <107285308+sandeepkumarreddymaram@users.noreply.github.com>
2023-02-20 14:57:21 -0600

    init commit

---
+++ src/main/java/com/example/firstspringlocal/FirstSpringLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringLocalApplication.class, args);
+    }
+
+}
